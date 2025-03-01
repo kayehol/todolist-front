984cffa9b071813910296ac9994536447f2bec9f
Kayê Holanda <kaye.holanda@gmail.com>
2025-03-01 00:22:08 -0300

    atualiza modal de dialogo

--- src/app/home/task-dialog/task-dialog.component.ts
+++ src/app/home/task-dialog/task-dialog.component.ts
@@ -1,9 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MAT_DIALOG_DATA, MatDialogModule, MatDialogRef } from '@angular/material/dialog';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
+import { Task } from '../task.interface';
 
 @Component({
   selector: 'app-task-dialog',
@@ -13,12 +16,16 @@ import { MatInputModule } from '@angular/material/input';
     ReactiveFormsModule,
     MatCardModule,
     MatInputModule,
+    MatDialogModule,
+    MatButtonModule,
   ],
   templateUrl: './task-dialog.component.html',
   styleUrl: './task-dialog.component.scss'
 })
 export class TaskDialogComponent {
   formTask: FormGroup;
+  readonly dialogRef = inject(MatDialogRef<TaskDialogComponent>);
+  readonly task = inject<Task>(MAT_DIALOG_DATA);
 
   constructor() {
     this.formTask = new FormGroup({
@@ -26,6 +33,16 @@ export class TaskDialogComponent {
       description: new FormControl<string>('', Validators.required),
       done: new FormControl<boolean>(false, Validators.required),
     });
+
+    if (this.task)
+      this.formTask.patchValue(this.task)
   }
 
+  salvar() {
+
+  }
+
+  cancelar() {
+    this.dialogRef.close();
+  }
 }
